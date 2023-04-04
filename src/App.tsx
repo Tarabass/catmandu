24f4954ba9cc3162b0a50eda0e7f867ff65951da
Tarabass <p.rietveld@live.com>
2023-04-04 11:39:27 +0200

    Implement BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,26 @@
-import React from 'react';
-import Navbar from './components/Navbar';
-import Home from './pages/Home';
-import Starred from './pages/Starred';
+import React from 'react'
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Starred from './pages/Starred'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
-function App() { 
-  const items = [{ text: 'Home', url: '/' }, { text: 'Starred', url: 'starred'}]
-  
-  return (
-    <div>
-      <Navbar title='Catmandu' items={items} />
-      <Home />
-      <Starred />
-    </div>
-  );
+function App() {
+	const items = [
+		{ text: 'Home', url: '/' },
+		{ text: 'Starred', url: 'starred' },
+	]
+
+	return (
+		<div>
+			<BrowserRouter>
+				<Navbar title="Catmandu" items={items} />
+				<Routes>
+					<Route path="/" Component={Home}></Route>
+					<Route path="/starred" Component={Starred}></Route>
+				</Routes>
+			</BrowserRouter>
+		</div>
+	)
 }
 
-export default App;
+export default App
