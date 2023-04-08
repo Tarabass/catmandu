b733f60488f934eb130ed4a2c49f4a8b4c1629a2
Tarabass <p.rietveld@live.com>
2023-04-08 15:27:52 +0200

    Add 404 route to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Starred from './pages/Starred'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 function App() {
-	const items = [
+	const navbarItems = [
 		{ text: 'Home', url: '/' },
 		{ text: 'Starred', url: 'starred' },
 	]
 
 	return (
-		<div>
+		<>
 			<BrowserRouter>
-				<Navbar title="Catmandu" items={items} />
+				<Navbar title="Catmandu" items={navbarItems} />
 				<Routes>
 					<Route path="/" Component={Home}></Route>
 					<Route path="/starred" Component={Starred}></Route>
+					<Route path="*" element={<h1>Not found!</h1>} />
 				</Routes>
 			</BrowserRouter>
-		</div>
+		</>
 	)
 }
 
