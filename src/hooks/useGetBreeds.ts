0be692f75505f0dc0f51c8762d07d66ee126b0c2
Tarabass <p.rietveld@live.com>
2023-04-08 21:50:02 +0200

    Create hook for fetching breeds

---
+++ src/hooks/useGetBreeds.ts
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import { Breed } from '../types/types'
+import axios from 'axios'
+
+const useGetBreeds = () => {
+	const [breeds, setBreeds] = useState(Array<Breed>)
+
+	const fetchData = async () => {
+		try {
+			const res = await axios.get(
+				`${process.env.REACT_APP_API_ENDPOINT}/breeds`,
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						'x-api-key': process.env.REACT_APP_API_KEY,
+					},
+				}
+			)
+
+			setBreeds(res.data || [])
+		} catch (error) {
+			// Handle errors
+			throw error
+		}
+	}
+
+	useEffect(() => {
+		fetchData()
+	}, [])
+
+	return { breeds }
+}
+
+export default useGetBreeds
