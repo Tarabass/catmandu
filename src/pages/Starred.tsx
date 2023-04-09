3ac5e742afad6780aa443cfd29612c6b6639de9a
Tarabass <p.rietveld@live.com>
2023-04-09 13:10:14 +0200

    Show starred cats from state on starred pages

--- src/pages/Starred.tsx
+++ src/pages/Starred.tsx
@@ -1,13 +1,34 @@
-import { ComponentType } from 'react'
+import { FC } from 'react'
 import { useRecoilValue } from 'recoil'
 import starredCatsState from '../state/selectors/starredCatsState'
+import { StarredCat } from '../types/types'
 
-const Starred: ComponentType = () => {
+const Starred: FC = () => {
 	const starredCats = useRecoilValue(starredCatsState)
-	console.log(starredCats);
-	
 
-	return <div>Starred</div>
+	return (
+		<div className="page">
+			<div className="container">
+				<section className="card__wrap--outer">
+					{starredCats.map((starredCat: StarredCat) => (
+						<article
+							className="card__wrap--inner"
+							key={starredCat.id}
+						>
+							<div className="card">
+								<img
+									alt=""
+									src={starredCat.image.url}
+									width="10%"
+									height="10%"
+								/>
+							</div>
+						</article>
+					))}
+				</section>
+			</div>
+		</div>
+	)
 }
 
 export default Starred
--- src/types/types.ts
+++ src/types/types.ts
@@ -26,3 +26,15 @@ export type Cat = {
 	width: number
 	height: number
 }
+
+export type StarredCat = {
+	id: string
+	user_id: string
+	image_id: string
+	sub_id: string
+	created_at: string
+	image: {
+		id: string
+		url: string
+	}
+}
