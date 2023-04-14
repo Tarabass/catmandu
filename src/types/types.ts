cec7b4dcb542ee0714ad4ff7f0c65ac22f519628
Tarabass <p.rietveld@live.com>
2023-04-14 22:19:24 +0200

    Create API typescript types

--- src/types/types.ts
+++ src/types/types.ts
@@ -15,9 +15,51 @@ export enum Order {
 	RAND = 'RAND',
 }
 
+// From Cat API
 export type Breed = {
 	id: string
 	name: string
+	reference_image_id: string
+	// image: {
+	// 	id: string
+	// 	url: string
+	// 	width: number
+	// 	height: number
+	// }
+	image: Cat
+}
+
+export type Category = {
+	id: number
+	name: string
+}
+
+export type Vote = {
+	id: number
+	user_id: string
+	image_id: string
+	sub_id: string
+	created_at: string
+	value: number
+	country_code: string
+	// image: {
+	// 	id: string
+	// 	url: string
+	// }
+	image: Cat
+}
+
+export type StarredCat = {
+	id: number
+	user_id: string
+	image_id: string
+	sub_id: string
+	created_at: string
+	// image: {
+	// 	id: string
+	// 	url: string
+	// }
+	image: Cat
 }
 
 export type Cat = {
@@ -25,35 +67,14 @@ export type Cat = {
 	url: string
 	width: number
 	height: number
+	breeds: Array<Breed>
 }
 
-export type StarredCat = {
-	id: string
-	user_id: string
-	image_id: string
-	sub_id: string
+export type UploadedCat = Cat & {
+	breed_ids: string
 	created_at: string
-	image: {
-		id: string
-		url: string
-	}
-}
-
-export type UploadedCat = {
-	breeds_ids: string
-	breeds: Array<Object>
-	created_at: string
-	favourite: {
-		id: string
-	}
-	height: number
-	id: string
 	original_filename: string
 	sub_id: string
-	url: string
-	vote: {
-		id: string
-		value: number
-	}
-	width: number
+	vote: Vote
+	favourite: StarredCat
 }
