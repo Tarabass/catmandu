20cb01d37c612eb5864ff17480692aa1dd9033c0
Tarabass <p.rietveld@live.com>
2023-04-20 15:07:23 +0200

    Add suspense to prevent react render warning

--- src/pages/Favourites.tsx
+++ src/pages/Favourites.tsx
@@ -1,36 +1,14 @@
 import { FC, Suspense } from 'react'
-import { Favourite } from '../types/types'
-import { useRecoilValue } from 'recoil'
-import favouritesState from '../state/atoms/favouritesState'
+import Favourites from '../components/Favourites'
 
-const Favourites: FC = () => {
-	const favourites = useRecoilValue(favouritesState)
-
+const FavouritesPage: FC = () => {
 	return (
-		<div className="page">
-			<div className="container">
-				<section className="card__wrap--outer">
-				<Suspense fallback={<div>Loading..</div>}>
-					{favourites.map((favourite: Favourite) => (
-						<article
-							className="card__wrap--inner"
-							key={favourite.id}
-						>
-							<div className="card">
-								<img
-									alt=""
-									src={favourite.image.url}
-									width="10%"
-									height="10%"
-								/>
-							</div>
-						</article>
-					))}
-					</Suspense>
-				</section>
-			</div>
-		</div>
+		<>
+			<Suspense fallback={<div>Loading..</div>}>
+				<Favourites />
+			</Suspense>
+		</>
 	)
 }
 
-export default Favourites
+export default FavouritesPage
