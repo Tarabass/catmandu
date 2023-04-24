081fb9c5bc43d71a9f47b59868ae518244cc77d4
Tarabass <p.rietveld@live.com>
2023-04-24 12:28:49 +0200

    Change limit filter from number to select

--- src/components/LimitFilter.tsx
+++ src/components/LimitFilter.tsx
@@ -5,25 +5,26 @@ import { FilterProps } from '../types/types'
 
 const LimitFilter: FC<FilterProps> = ({ field }) => {
 	const [filter, setFilter] = useRecoilState(filterState)
+	const limitArray = [5, 10, 25]
 
-	const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-		// TODO: Set page given on current limit settings
+	const onChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
 		setFilter({ ...filter, [field]: event.target.value })
-		// onUpdate()
 	}
 
 	return (
 		<div>
-			<label htmlFor={field}>{field.toUpperCase()}: </label>
-			<input
+			<select
 				id={field}
 				name={field}
-				type="number"
-				min="1"
-				max="10"
 				value={filter['limit']}
 				onChange={onChange}
-			/>
+			>
+				{limitArray.map((limit) => (
+					<option key={limit} value={limit} selected={limit === 10}>
+						{limit}
+					</option>
+				))}
+			</select>
 		</div>
 	)
 }
